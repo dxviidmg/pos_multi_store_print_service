cdea460afd6bbfaa28317b35cd4ebfe4c8da7d2a
David <dmg_92@hotmail.com>
2025-03-17 19:26:39 -0600

    fix

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ async def post_ticket(request: Request):
         printer.text(formatted_date + "\n\n")
 
         # Imprimir el nombre del cliente, si existe
-        if 'full_name' in data['client']:
+        if data['client'] and 'full_name' in data['client']:
             printer.text("Cliente: " + data['client']['full_name'] + "\n\n")
 
         # Imprimir tabla de productos
