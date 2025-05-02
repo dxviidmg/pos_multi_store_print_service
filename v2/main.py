c02268fed764290138e9846360dadd44e0ea68e4
David <dmg_92@hotmail.com>
2025-05-02 12:16:25 -0600

    product 9

--- v2/main.py
+++ v2/main.py
@@ -124,7 +124,7 @@ async def post_ticket(request: Request):
 
         for product in products:
             qty = product["quantity"]
-            name = str(product["name"])[:16].ljust(16)
+            name = str(product["name"])[:9].ljust(9)
             price = float(product["price"])
             total = qty * price
             line = f"{qty:<7} | {name} | {total:7.2f}"
