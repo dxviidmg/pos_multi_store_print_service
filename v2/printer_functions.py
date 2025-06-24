4ae2fbc04a6db755ca2f450145cbd9191b8ef60e
David <dmg_92@hotmail.com>
2025-06-24 17:34:47 -0600

    SPACING

--- v2/printer_functions.py
+++ v2/printer_functions.py
@@ -2,61 +2,57 @@ import win32print
 import win32ui
 import win32con
 
+
 from utils import get_store_printer_data
 
-# Constantes de formato
-FONT_CONFIG = {"name": "Arial", "height": 12, "weight": 600}
+# Formato general de la fuente
+FORMAT = {"name": "Arial", "height": 12, "weight": 600}
 Y_INIT = 20
 SPACING = 40
 
 printer_name = win32print.GetDefaultPrinter()
 
-def start_printing(title="Ticket Python", data={}):
+def start_printing(titulo="Ticket Python", data={}):
     hDC = win32ui.CreateDC()
     hDC.CreatePrinterDC(printer_name)
-    hDC.StartDoc(title)
+    hDC.StartDoc(titulo)
     hDC.StartPage()
-
-    font = win32ui.CreateFont(FONT_CONFIG)
+    font = win32ui.CreateFont(FORMAT)
     hDC.SelectObject(font)
 
-    y = print_store_info(hDC, data)
-    return hDC, y
-
-def print_store_info(hDC, data):
     store_printer = get_store_printer_data(data)
-    print(store_printer)  # Supongo es para debug
+    print(store_printer)
 
     y = Y_INIT
+    hDC.TextOut(0, y, store_printer['store']['tenant_name'])
+    y += SPACING
 
-    def print_line_if_exists(value):
-        nonlocal y
-        if value:
-            hDC.TextOut(0, y, value)
-            y += Y_INIT
-
-    print_line_if_exists(store_printer['store']['tenant_name'])
-    print_line_if_exists(store_printer['store']['address'])
-    print_line_if_exists(store_printer['store']['phone_number'])
-
-    y += Y_INIT  # Espacio adicional después del encabezado
-    return y
-
-def print_lines(hDC, lines, y_start, is_test=False):
-    y = y_start
-
-    if is_test:
-        max_width = hDC.GetDeviceCaps(win32con.HORZRES)
-        hDC.TextOut(0, y, f'Ancho máximo: {max_width}px.')
-        return hDC
-
-    for line in lines:
-        hDC.TextOut(0, y, line)
+    if store_printer['store']['address']:
+        hDC.TextOut(0, y, store_printer['store']['address'])
         y += SPACING
 
+    if store_printer['store']['phone_number']:
+        hDC.TextOut(0, y, store_printer['store']['phone_number'])
+        y += SPACING
     return hDC, y
+
+
 
 def end_printing(hDC):
     hDC.EndPage()
     hDC.EndDoc()
     hDC.DeleteDC()
+
+
+def print_lines(hDC, lineas, y_inicio, is_test=False):
+    if is_test:
+        max_width = hDC.GetDeviceCaps(win32con.HORZRES)
+        y_inicio += Y_INIT
+        hDC.TextOut(0, y_inicio, 'Ancho maximo: ' + str(max_width) + 'pxs.')
+        return hDC
+    else:
+        y = y_inicio
+        for linea in lineas:
+            hDC.TextOut(0, y, linea)
+            y += SPACING
+        return hDC, y
