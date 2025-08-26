bfd7cf3d08cd5358a7faf60d2ab6ddeffcb4ce3b
David <dmg_92@hotmail.com>
2025-08-26 12:22:04 -0600

    cambio

--- v2/main.py
+++ v2/main.py
@@ -83,7 +83,10 @@ async def post_ticket(request: Request):
         y += SPACING
         hDC.TextOut(0, y, f"Total: ${float(data['total']):.2f}")
         y += SPACING
-
+        hDC.TextOut(0, y, f"Pagó con: ${float(data['paymemt']['paidWith']):.2f}")
+        y += SPACING
+        hDC.TextOut(0, y, f"Cambio: ${float(data['payment']['change']):.2f}")
+        y += SPACING
         if data.get('reservation_in_progress'):
             hDC.TextOut(0, y, f"Pagado: ${float(data['paid']):.2f}")
             y += SPACING
